bfa685d4c17700dc512cacb2037a7af343ed917a
Shoxruh Rustamaliyev <shoxruhrustamaliyev@ya.ru>
2023-06-11 13:06:21 +0500

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import heroImage from './assets/hero.svg'
+import firstExampleImage from './assets/example-1.svg'
+import secondExampleImage from './assets/example-2.svg'
+import thirdExampleImage from './assets/example-3.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className='container mx-auto'>
+    <div className='container 2xl:px-48'>
       <nav className='flex justify-between items-center my-10'>
         <a href="/" className='font-nunito font-bold text-2xl'>Plant</a>
 
@@ -26,7 +29,7 @@ function App() {
       </nav>
 
       <header className='flex justify-between gap-x-28 items-center'>
-        <section className='w-[548px]'>
+        <section className='max-w-xl'>
           <h1 className='text-7xl font-nunito font-bold leading-[6rem]'>Growing Beautiful Plants at Home</h1>
 
           <p className='mt-6 mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eu, tempor, accumsan sit amet nunc cursus. Nec tristique at in erat lectus mas sa diam. Lectus elit, nulla elementum fringilla at.</p>
@@ -54,6 +57,19 @@ function App() {
 
         <img src={heroImage} alt="" className='w-fit' />
       </header>
+
+      <section className='flex gap-x-16 mt-24'>
+        <div>
+          <h2 className='max-w-sm text-4xl font-nunito mb-8'>We have lots of plants collection for you and your family</h2>
+          <a href="#" className='text'>See all collection &rarr;</a>
+        </div>
+
+        <div className='flex gap-x-6'>
+          <img src={firstExampleImage} alt="" className='bg-[#F6EDE8] px-5 py-6' />
+          <img src={secondExampleImage} alt="" className='bg-[#F0F1F6] px-5 py-6' />
+          <img src={thirdExampleImage} alt="" className='bg-[#F6EDE8] px-5 py-6' />
+        </div>
+      </section>
     </div>
   )
 }
